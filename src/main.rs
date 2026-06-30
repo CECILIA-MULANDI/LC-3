e7bfecd7d6a372bb8d110f22c097b4e062c7954a
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2026-06-30 19:22:40 +0300

    feat(main): load program image into VM memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod hardware;
+use byteorder::{BigEndian, ReadBytesExt};
+use hardware::vm::VM;
+
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let mut vm = VM::new();
+    let mut f = File::open("program.obj").expect("could not open file");
+    let base_address = f.read_u16::<BigEndian>().expect("error");
+    let mut address = base_address as usize;
+    loop {
+        match f.read_u16::<BigEndian>() {
+            Ok(instruction) => {
+                vm.write_memory(address, instruction);
+                address += 1;
+            }
+            Err(e) => {
+                if e.kind() == std::io::ErrorKind::UnexpectedEof {
+                    println!("OK")
+                } else {
+                    panic!("Error reading file: {}", e);
+                }
+                break;
+            }
+        }
+    }
 }
